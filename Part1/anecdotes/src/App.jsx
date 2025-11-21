84b1418dd0eaf834b139a079e9238d6649cc01b0
Elsa Abbas <eabbas@berkeley.edu>
2025-11-20 19:15:31 -0800

    Add completed code for anecdotes exercise

---
+++ Part1/anecdotes/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const zeros = new Array(anecdotes.length).fill(0);
+
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(zeros)
+  const [maxVotesIdx, setMaxVotesIdx] = useState(0)
+
+  const handleNextClick = () => {
+    const max = anecdotes.length
+    const random = Math.floor(Math.random() * max);
+    setSelected(random)
+  }
+
+  const handleVotesClick = () => {
+    const newVotes = [...votes] //create a copy so not directly mutating state
+    newVotes[selected] += 1
+    setVotes(newVotes)
+
+    // get new max
+    if (newVotes[selected] > newVotes[maxVotesIdx]) {
+      setMaxVotesIdx(selected)
+    }
+    // const newMaxVotes = Math.max(newVotes[selected], newVotes[maxVotesIdx])
+    // console.log("max votes ", newMaxVotes)
+    // const newMaxVotesIdx = newVotes.indexOf(newMaxVotes)
+    // setMaxVotesIdx(newMaxVotesIdx)
+  }
+  // console.log(maxVotesIdx)
+
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+
+      <button onClick={handleNextClick}>
+        next anecdote
+      </button>
+
+      <button onClick={handleVotesClick}>
+        vote
+      </button>
+
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[maxVotesIdx]}</p>
+      <p>has {votes[maxVotesIdx]} votes</p>
+    </div>
+  )
+}
+
+export default App
